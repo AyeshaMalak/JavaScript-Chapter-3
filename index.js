ed1253482329558322717351437ae05757d7eca6
AyeshaMalak <ayeshamalak34@gmail.com>
2024-08-12 11:37:22 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+//Chapter #3
+
+var age = 16;
+alert("I am "+ age + " years old");
+
+var visitors = 24;
+alert(" You have visited this site " + visitors + " times ");
+
+var birthYear = 2007;
+document.write(' My Birth Year is ' +birthYear+ '<br/>  Data type of my declared variable is number ');
+
+var vistor = "Ayesha";
+var quantity = "10";
+var product = "T-shirts";
+document.write(visitor + " Ordered " + quantity + product + " On XYZ Clothing Store ");
